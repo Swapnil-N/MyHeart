9a765f401390188fe796d3a42dbf6d365936a440
Swapnil N <swapnil.napuri@gmail.com>
2018-09-09 04:39:12 -0400

    set up fragments

--- app/src/main/java/com/example/mlh_admin/myheart/MainActivity.java
+++ app/src/main/java/com/example/mlh_admin/myheart/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.mlh_admin.myheart;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    DatabaseReference database;
+    DatabaseReference userData;
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance().getReference();
+
+        loadFragment(new HistoryFragment());
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_history:
+                fragment = new HistoryFragment();
+                break;
+            case R.id.navigation_tracker:
+                fragment = new TrackerFragment();
+                break;
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
     }
 }
